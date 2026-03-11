b966b866baf6c91c14ded6ee0a8106d9888c4c05
Semyon <normal.man101@outlook.com>
2026-03-11 15:48:25 +0300

    Начал переносить логику в классы

---
+++ GamesWebApiBelyshevSemyon.Core/GamesWebApiBelyshevSemyon.Core/Classes/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace GamesWebApiBelyshevSemyon.Core.Classes;
+
+public static class Utility
+{
+    public static bool IsEmpty(string variable)
+    {
+        return (variable.Length == 0);
+    }
+
+    public static bool IsNull<T>(T variable)
+    {
+        return (variable == null);
+    }
+
+    public static void UpdateDataFile<T>(string filePath, T data) where T : class
+    {
+        var option = new JsonSerializerOptions { WriteIndented = true };
+        File.WriteAllText(filePath, JsonSerializer.Serialize(data, option));
+    }
+}
--- GamesWebApiBelyshevSemyon.Core/GamesWebApiBelyshevSemyon.Core/Program.cs
+++ GamesWebApiBelyshevSemyon.Core/GamesWebApiBelyshevSemyon.Core/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using GamesWebApiBelyshevSemyon.Core;
+using GamesWebApiBelyshevSemyon.Core.Classes;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
@@ -26,95 +27,19 @@ var jsonResults = File.ReadAllText(jsonResultsPath);
 var competitions = JsonSerializer.Deserialize<List<Competition>>(jsonCompetitions);
 var results = JsonSerializer.Deserialize<List<Result>>(jsonResults);
 
-bool IsEmpty(string variable)
-{
-    return (variable.Length == 0);
-}
-
-bool IsNull<T>(T variable)
-{
-    return (variable == null);
-}
-
-void UpdateDataFile<T>(string filePath, T data) where T : class
-{
-    var option = new JsonSerializerOptions { WriteIndented = true };
-    File.WriteAllText(filePath, JsonSerializer.Serialize(data, option));
-}
-
 #endregion
 
 #region Методы соревнований
 
 app.MapGet("/api/competitions", () => competitions);
 
-app.MapGet("/api/competitions/{id:guid}", (Guid id) =>
-{
-    var foundCompetition = competitions!.FirstOrDefault(competition => competition.Id == id);
+app.MapGet("/api/competitions/{id:guid}", CompetitionsUtility.GetById).Produces<Competition>(StatusCodes.Status200OK).Produces(StatusCodes.Status404NotFound);
 
-    if (foundCompetition == null) Results.NotFound("Не найдено соревнование");
+app.MapPost("/api/competitions", CompetitionsUtility.Add).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status201Created);
 
-    return foundCompetition;
-}).Produces<Competition>(StatusCodes.Status200OK).Produces(StatusCodes.Status404NotFound);
+app.MapPut("/api/competitions/{id:guid}", CompetitionsUtility.UpdateWithNew).Produces(StatusCodes.Status404NotFound).Produces(StatusCodes.Status201Created);
 
-app.MapPost("/api/competitions", (Competition competition) =>
-{
-    if (IsEmpty(competition.Name)) return Results.BadRequest("Отсутствует имя");
-    if (competition.Date == DateTime.MinValue) return Results.BadRequest("Отсутствует дата");
-    if (IsEmpty(competition.Location)) return Results.BadRequest("Отсутствует локация");
-    if (IsEmpty(competition.SportType)) return Results.BadRequest("Отсутствует вид спорта");
-
-    var newCompetition = competition with { Id = Guid.NewGuid(), IsDeleted = false };
-
-    competitions!.Add(newCompetition);
-
-    UpdateDataFile(jsonCompetitionsPath, competitions);
-
-    return Results.Created();
-}).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status201Created);
-
-app.MapPut("/api/competitions/{id:guid}", (Guid id, Competition competition) =>
-{
-    var foundCompetition = competitions!.FirstOrDefault(existingCompetition => existingCompetition.Id == id);
-
-    if (IsNull(foundCompetition)) return Results.NotFound();
-
-    competitions!.RemoveAll(existingCompetition => existingCompetition.Id == id);
-
-    var newName = IsEmpty(competition.Name) ? foundCompetition!.Name : competition.Name;
-    var newDate = competition.Date == DateTime.MinValue ? foundCompetition!.Date : competition.Date;
-    var newLocation = IsEmpty(competition.Location) ? foundCompetition!.Location : competition.Location;
-    var newSportType = IsEmpty(competition.SportType) ? foundCompetition!.SportType : competition.SportType;
-
-    var updatedCompetition = competition with
-    {
-        Name = newName,
-        Date = newDate,
-        Location = newLocation,
-        SportType = newSportType
-    };
-
-    competitions.Add(updatedCompetition);
-
-    UpdateDataFile(jsonCompetitionsPath, competitions);
-
-    return Results.Created();
-}).Produces(StatusCodes.Status404NotFound).Produces(StatusCodes.Status201Created);
-
-app.MapDelete("/api/competitions/{id:guid}", (Guid id) =>
-{
-    var foundCompetition = competitions!.FirstOrDefault(competition => competition.Id == id);
-
-    if (IsNull(foundCompetition)) return Results.NotFound("Не найдено соревнование");
-
-    competitions!.RemoveAll(existingCompetition => existingCompetition.Id == id);
-
-    competitions.Add(foundCompetition! with { IsDeleted = true });
-
-    UpdateDataFile(jsonCompetitionsPath, competitions);
-
-    return Results.Ok();
-}).Produces(StatusCodes.Status404NotFound).Produces(StatusCodes.Status200OK);
+app.MapDelete("/api/competitions/{id:guid}", CompetitionsUtility.DeleteById).Produces(StatusCodes.Status404NotFound).Produces(StatusCodes.Status200OK);
 
 #endregion
 
@@ -126,7 +51,7 @@ app.MapGet("/api/results/{id:guid}", (Guid id) =>
 {
     var foundResult = results!.FirstOrDefault(result => result.Id == id);
 
-    if (IsNull(foundResult)) return foundResult;
+    if (Utility.IsNull(foundResult)) return foundResult;
 
     Results.NotFound("Не найден результат");
     return null;
@@ -144,9 +69,9 @@ app.MapGet("/api/competitions/{competitionId:guid}/results", (Guid competitionId
 
 app.MapPost("/api/results", (Result result) =>
 {
-    if (competitions!.Any(competition => competition.Id != result.Id))
+    if (competitions!.Any(competition => competition.Id != result.CompetitionId))
         return Results.BadRequest("Несуществующий идентификатор соревнования");
-    if (IsEmpty(result.ParticipantName)) return Results.BadRequest("Отсутствует имя");
+    if (Utility.IsEmpty(result.ParticipantName)) return Results.BadRequest("Отсутствует имя");
     if (result.Place <= 0) return Results.BadRequest("Место не больше нуля");
     if (result.Score <= 0) return Results.BadRequest("Очки не больше нуля");
 
@@ -161,7 +86,7 @@ app.MapPost("/api/results", (Result result) =>
 
     results!.Add(newResult);
 
-    UpdateDataFile(jsonResultsPath, results);
+    Utility.UpdateDataFile(jsonResultsPath, results);
 
     return Results.Created();
 }).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status201Created);
@@ -170,13 +95,13 @@ app.MapPut("/api/results/{id}", (Guid id, Result result) =>
 {
     var foundResult = results!.FirstOrDefault(existingResult => existingResult.Id == id);
 
-    if (IsNull(foundResult)) return Results.NotFound("Не найден результат");
+    if (Utility.IsNull(foundResult)) return Results.NotFound("Не найден результат");
 
     results!.RemoveAll(existingResult => existingResult.Id == id);
 
     var newCompetitionId =
-        IsEmpty(result.CompetitionId.ToString()) ? foundResult!.CompetitionId : result.CompetitionId;
-    var newParticipantName = IsEmpty(result.ParticipantName) ? foundResult!.ParticipantName : result.ParticipantName;
+        Utility.IsEmpty(result.CompetitionId.ToString()) ? foundResult!.CompetitionId : result.CompetitionId;
+    var newParticipantName = Utility.IsEmpty(result.ParticipantName) ? foundResult!.ParticipantName : result.ParticipantName;
     var newPlace = result.Place > 0 ? result.Place : foundResult!.Place;
     var newScore = result.Score > 0 ? result.Score : foundResult!.Score;
 
@@ -190,7 +115,7 @@ app.MapPut("/api/results/{id}", (Guid id, Result result) =>
 
     results.Add(updatedResult);
 
-    UpdateDataFile(jsonResultsPath, results);
+    Utility.UpdateDataFile(jsonResultsPath, results);
 
     return Results.Ok();
 }).Produces(StatusCodes.Status404NotFound).Produces(StatusCodes.Status200OK);
@@ -199,13 +124,13 @@ app.MapDelete("/api/results/{id:guid}", (Guid id) =>
 {
     var foundResult = results!.FirstOrDefault(result => result.Id == id);
 
-    if (IsNull(foundResult)) return Results.NotFound("Не найден результат");
+    if (Utility.IsNull(foundResult)) return Results.NotFound("Не найден результат");
 
     results!.RemoveAll(existingResults => existingResults.Id == id);
 
     results.Add(foundResult! with { IsDeleted = true });
 
-    UpdateDataFile(jsonResultsPath, results);
+    Utility.UpdateDataFile(jsonResultsPath, results);
 
     return Results.Ok();
 }).Produces(StatusCodes.Status404NotFound).Produces(StatusCodes.Status200OK);
